75a11a2c1af4430a9c7a4326a02e0833bdfe1052
Gaurav Dahal <gauravdahal@Gauravs-MacBook-Air.local>
2023-04-25 13:12:54 +1000

    convert calculator in progress

--- app/src/main/java/com/gaurav/budgetplanner/Features/Converter/CurrencyConvertActivity.kt
+++ app/src/main/java/com/gaurav/budgetplanner/Features/Converter/CurrencyConvertActivity.kt
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gaurav.budgetplanner.R
 import com.gaurav.budgetplanner.databinding.ActivityCurrencyConvertBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 
 class CurrencyConvertActivity : AppCompatActivity() {
     private var _binding:ActivityCurrencyConvertBinding?= null
     private val binding get() = _binding!!
+    private var currentNumber:String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,55 @@ class CurrencyConvertActivity : AppCompatActivity() {
 
         binding.clDetails2.setOnClickListener {
             openCountryList()
+        }
+
+        binding.zero.setOnClickListener {
+            if(currentNumber.isNotEmpty()){
+                setNumberOnHeader("0")
+            }
+        }
+
+        binding.one.setOnClickListener {
+            setNumberOnHeader("1")
+        }
+
+        binding.two.setOnClickListener {
+            setNumberOnHeader("2")
+        }
+
+        binding.three.setOnClickListener {
+            setNumberOnHeader("3")
+        }
+
+        binding.four.setOnClickListener {
+            setNumberOnHeader("4")
+        }
+
+        binding.five.setOnClickListener {
+            setNumberOnHeader("5")
+        }
+        binding.six.setOnClickListener {
+            setNumberOnHeader("6")
+        }
+
+        binding.seven.setOnClickListener {
+            setNumberOnHeader("7")
+        }
+
+        binding.eight.setOnClickListener {
+            setNumberOnHeader("8")
+        }
+
+        binding.nine.setOnClickListener {
+            setNumberOnHeader("8")
+        }
+
+    }
+
+    private fun setNumberOnHeader(number:String){
+        if(currentNumber.length<=15){
+            currentNumber += number
+            binding.fromValueCurr.text = NumberFormat.getNumberInstance(Locale.getDefault()).format(currentNumber.toLong())
         }
     }
 
