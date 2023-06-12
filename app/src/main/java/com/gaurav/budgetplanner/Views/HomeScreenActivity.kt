1bce2904e590770013ce53558c974b3092940d24
Gaurav Dahal <gauravdahal@Gauravs-MacBook-Air.local>
2023-06-12 21:29:59 +1000

    work in progress

--- app/src/main/java/com/gaurav/budgetplanner/Views/HomeScreenActivity.kt
+++ app/src/main/java/com/gaurav/budgetplanner/Views/HomeScreenActivity.kt
@@ -3,10 +3,15 @@ package com.gaurav.budgetplanner.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gaurav.budgetplanner.R
+import com.gaurav.budgetplanner.Views.Activity.BaseActivity
+import com.gaurav.budgetplanner.databinding.ActivityHomeScreenBinding
 
-class HomeScreenActivity : AppCompatActivity() {
+class HomeScreenActivity : BaseActivity() {
+    private var _binding:ActivityHomeScreenBinding?=null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        _binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/gaurav/budgetplanner/Views/SplashScreen.kt
@@ -0,0 +1,76 @@
+package com.gaurav.budgetplanner.Views
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import androidx.core.os.postDelayed
+import com.gaurav.budgetplanner.OnBoardActivity
+import com.gaurav.budgetplanner.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var splashAnimation: Animation
+    private lateinit var secondAnimation: Animation
+    private var _binding:ActivitySplashScreenBinding?=null
+    private val binding get() = _binding!!
+    private lateinit var splashIcon: ImageView
+    val scaleDownFactor = 0.7f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        splashIcon = binding.splashIcon
+        init()
+    }
+
+    private fun init(){
+        val width = splashIcon.width
+        val height = splashIcon.height
+        splashAnimation = AnimationUtils.loadAnimation(
+            this,
+            com.gaurav.budgetplanner.R.anim.splash_icon_animation
+        )
+        secondAnimation = AnimationUtils.loadAnimation(
+            this,
+            com.gaurav.budgetplanner.R.anim.splash_second_animation
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            splashIcon.startAnimation(splashAnimation)
+
+        }, 1000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val currentWidth = splashIcon.width
+            val currentHeight = splashIcon.height
+
+            val scaledWidth = (currentWidth * scaleDownFactor).toInt()
+            val scaledHeight = (currentHeight * scaleDownFactor).toInt()
+
+            // Set the scaled dimensions for the view
+            splashIcon.layoutParams.width = scaledWidth
+            splashIcon.layoutParams.height = scaledHeight
+            splashIcon.requestLayout()
+
+            splashIcon.startAnimation(secondAnimation)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this,OnBoardActivity::class.java))
+
+            }, 300)
+
+        }, 4000)
+
+
+
+
+
+    }
+}
