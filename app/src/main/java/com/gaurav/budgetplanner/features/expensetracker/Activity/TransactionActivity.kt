60819b8cf21ca0aa28030deebd8e8dd747b7a962
Gaurav Dahal <gauravdahal@Gauravs-MacBook-Air.local>
2023-06-28 18:18:03 +1000

    adaptive spacing issue fixed

--- app/src/main/java/com/gaurav/budgetplanner/features/expensetracker/Activity/TransactionActivity.kt
+++ app/src/main/java/com/gaurav/budgetplanner/features/expensetracker/Activity/TransactionActivity.kt
@@ -1,16 +1,37 @@
 package com.gaurav.budgetplanner.features.expensetracker.Activity
 
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.gaurav.budgetplanner.R
 import com.gaurav.budgetplanner.Views.Activity.BaseActivity
 import com.gaurav.budgetplanner.databinding.ActivityTransactionBinding
+import com.gaurav.budgetplanner.features.expensetracker.Adapters.AdaptiveSpacingItemDecoration
 import com.gaurav.budgetplanner.features.expensetracker.Adapters.CategoryAdapter
 
 
 class TransactionActivity : BaseActivity() {
     private var _binding:ActivityTransactionBinding?=null
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView:RecyclerView
+
+    private var categories:Map<String,Int> =
+        mapOf("Transportation" to R.drawable.img_transport,
+        "Workout" to R.drawable.img_gym,
+            "Family" to R.drawable.img_family,
+            "Groceries" to R.drawable.img_groceries,
+            "Gifts" to R.drawable.img_gifts,
+            "More" to R.drawable.img_add)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityTransactionBinding.inflate(layoutInflater)
@@ -22,12 +43,25 @@ class TransactionActivity : BaseActivity() {
     }
 
     private fun setRecyclerView(){
-        val adapter = CategoryAdapter()
-        binding.rvCategories.layoutManager = GridLayoutManager(this,3)
-        binding.rvCategories.isNestedScrollingEnabled=false
-        binding.rvCategories.adapter = adapter
+        recyclerView = binding.rvCategories
+        val adapter = CategoryAdapter(categories)
+        recyclerView.layoutManager = StaggeredGridLayoutManager(3,1)
+        recyclerView.isNestedScrollingEnabled=false
+        recyclerView.adapter = adapter
+//        recyclerView.addItemDecoration(AdaptiveSpacingItemDecoration(8,true));
+//        bindAdapter(recyclerView,8.0f,true)
 
     }
+
+
+//    @BindingAdapter(value = ["spacingSize", "spacingEdgeEnabled"], requireAll = true)
+//    fun bindAdapter(view: RecyclerView, spacingSize: Float, spacingEdgeEnabled: Boolean) {
+//        with(view) {
+//            addItemDecoration(
+//                AdaptiveSpacingItemDecoration(spacingSize.toInt(), spacingEdgeEnabled)
+//            )
+//        }
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
--- app/src/main/java/com/gaurav/budgetplanner/features/expensetracker/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/gaurav/budgetplanner/features/expensetracker/Adapters/CategoryAdapter.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
 import androidx.recyclerview.widget.RecyclerView
+import com.gaurav.budgetplanner.R
 import com.gaurav.budgetplanner.Utils.Constants
 import com.gaurav.budgetplanner.databinding.ItemTransactionCategoryBinding
 import com.gaurav.budgetplanner.features.converter.ViewModel.CountryViewModel
 import java.util.*
 
-class CategoryAdapter():RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+class CategoryAdapter(private val categories:Map<String,Int>):RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     var onItemClick : ((Map.Entry<String,String>) -> Unit)? = null
     private var selectedItemPosition:Int = -1
+
+    var data = categories.entries.toList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
@@ -23,15 +26,41 @@ class CategoryAdapter():RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return 6
+        return data.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val eachData = data[position]
+        holder.binding.tvTransaction.text = eachData.key
+        holder.binding.ivTransaction.setImageResource(eachData.value)
+        holder.binding.ivTransaction.setBackgroundResource(getRespectiveCircle(position))
 
+        holder.itemView.setOnClickListener {
+            selectedItemPosition = holder.adapterPosition
+            notifyDataSetChanged()
+        }
+
+        if(selectedItemPosition==position){
+            holder.itemView.setBackgroundColor(Color.parseColor("#0569FF"))
+        }
+        else{
+            holder.itemView.setBackgroundColor(Color.TRANSPARENT)
+        }
     }
 
 
     class ViewHolder(b: ItemTransactionCategoryBinding) : RecyclerView.ViewHolder(b.root) {
         var binding: ItemTransactionCategoryBinding = b
+    }
+
+    private fun getRespectiveCircle(index:Int):Int{
+        return when(index){
+            0 -> R.drawable.circle
+            1 -> R.drawable.circle_green
+            2 -> R.drawable.circle_red
+            3 -> R.drawable.circle_light_blue
+            4 -> R.drawable.circle_light_green
+            else -> R.drawable.circle_grey
+        }
     }
 }
